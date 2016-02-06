8100bfe4dc2a70a69d3b1e020d6fd16715638d46
madejean <madejean007@hotmail.fr>
2016-02-06 15:02:47 -0800

    taxes and tip calculator

--- discover_python/taxes_and_tip_calculator.py
+++ discover_python/taxes_and_tip_calculator.py
@@ -1,13 +1,14 @@
 """Hello my name is Marine,
 This program is written in Python to calculate the cost of your meal with taxes and tips included. This program will prevent you the struggles and stress of haaving to calculate the right answer when you go out, so enjoy! :) """
 
-print "Welcome to the taxes and tip calculator!"
-meal=float(raw_input("What is the price before tax? "))
+print "Welcome to the taxes and tip calculator!" #introduction
+#set variables with float and using raw_input
+meal=float(raw_input("What is the price before tax? ")) 
 tax=float (raw_input ("What are the taxes? (in %) "))
 tip=float (raw_input ("What do you want to tip? (in %) "))
 
 
-meal= meal+ (tax/100)*meal
-meal= meal+ ((tip/100)*meal)
+meal= meal+ (tax/100)*meal #change the meal variable by adding tax calculation
+meal= meal+ ((tip/100)*meal) #change the meal + tax variable by adding tip calculation
 
-print "The price you need to pay is: $%.6f." % meal
+print "The price you need to pay is: $%.6f." % meal #prints the total cost 
