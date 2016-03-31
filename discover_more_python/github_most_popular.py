6e57ceebfd9f3c71fbbb3b5f829c34b38eb798a3
madejean <madejean007@hotmail.fr>
2016-03-31 19:35:37 +0000

    first commit

--- discover_more_python/github_most_popular.py
+++ discover_more_python/github_most_popular.py
@@ -1,7 +1,10 @@
-import requests
+from urllib2 import Request, urlopen, URLError
 request_headers = {
     'User-Agent': 'Holberton_School',
     'Authorization':'token 6e7f3146a1a9d7a12e4aceac3f3918c2fa57be6a'
-}
-r = requests.get('https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc')
-r.text
+    }
+url = Request("https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc")
+r = urlopen(url)
+response = r.read()
+print response
+
