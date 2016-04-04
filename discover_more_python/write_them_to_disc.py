846f93f78b61616881436e831470c8c6a45a2cce
madejean <madejean007@hotmail.fr>
2016-04-04 04:51:21 +0000

    fixed authentification error

--- discover_more_python/write_them_to_disc.py
+++ discover_more_python/write_them_to_disc.py
@@ -4,7 +4,7 @@ request_headers = {
     'User-Agent': 'Holberton_School',
     'Authorization':'token 6e7f3146a1a9d7a12e4aceac3f3918c2fa57be6a'
     }
-url = Request("https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc")
+url = Request("https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc", None, request_headers)
 r = urlopen(url)
 response = r.read()
 
