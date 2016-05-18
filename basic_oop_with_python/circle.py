89d35eab114537bf326ea23d24f2361107f3f55f
madejean <madejean007@hotmail.fr>
2016-05-18 03:55:13 +0000

    updated Circle

--- basic_oop_with_python/circle.py
+++ basic_oop_with_python/circle.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python
+
 '''describe a Circle class with radius, center and color as private attributes and with a public name'''
 import math   
 class Circle:
-
-   def __init__(self, radius):
+    
+    '''Constructor'''
+    def __init__(self, radius):
       self.__radius = radius
-      
-#setter/getter to update and retreive private attributes
-   def get_color(self):
+      self.__center = [0,0]
+      self.__color = ""
+      self.name = ""
+
+    '''Destructor'''
+    def __del__(self):
+        pass  
+
+    '''setter/getter to update and retreive private attributes'''
+    def get_color(self):
        return self.__color
 
-   def set_color(self, color):
-      self.__color = color   
+    def set_color(self, color):
+        self.__color = color   
          
-   def get_center(self):
+    def get_center(self):
       return self.__center
     
-   def set_center(self, center):
-      self.__center = center
+    def set_center(self, center):
+        self.__center = center
               
-   def area(self):
-      return math.pi * ((self.__radius ** 2))
+    def area(self):
+        return math.pi * ((self.__radius ** 2))
                      
