1de98b568610fc3ed7c773872df5b4766105819a
madejean <madejean007@hotmail.fr>
2016-06-07 21:25:40 +0000

    added last name to select

--- basic_sql_playground/3-main.sql
+++ basic_sql_playground/3-main.sql
@@ -1,13 +1,13 @@
 UPDATE Person
 SET age = '27'
-WHERE first_name = 'Jon';
+WHERE first_name = 'Jon' AND last_name = 'Snow';
 
 UPDATE Person
 SET age = '18'
-WHERE first_name = 'Walter Junior';
+WHERE first_name = 'Walter Junior' AND last_name ='White';
 
 DELETE FROM Person
-WHERE first_name = 'Walter';
+WHERE first_name = 'Walter' AND last_name = 'White';
 
 DELETE FROM EyesColor
 WHERE person_id = 'Walter';
