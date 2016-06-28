951d64b159b7bf1ea3ac4d0c654026809ee4336b
Marine Dejean <marine@Admins-MacBook-Air.local>
2016-06-27 23:57:25 -0700

    task 5

--- divide_and_rule/h_sum.py
+++ divide_and_rule/h_sum.py
@@ -1,26 +1,53 @@
 import threading
 
-class Sum:
-
+class Sum():
+    total = 0
     def __init__(self, nb_threads, numbers):
+        self.threads = []
         self.nb_threads = nb_threads
         if not isinstance(nb_threads, int):
             raise Exception("nb_threads is not an integer")
         self.numbers = numbers
         if not isinstance(numbers, list):
             raise Exception("numbers is not an array of integers")
+        Sum.total = 0
+
+        if len(numbers) >= nb_threads:
+            part = nb_threads
+        else:
+            part = len(numbers)
+        div = [len(numbers) // (part)] * part
+        sub = len(numbers)-sum(div)
+        div[:sub] = [i + 1 for i in div[:sub]]
+        List = [numbers[sum(div[:d]):sum(div[:d+1])] for d in range(part)]
+        for part in List:
+            thread = SumThread(part)
+            self.threads += [thread]
+            thread.start()
 
     def isComputing(self):
+        for thread in self.threads:
+            if thread.isAlive():
+                return True
+        return False
 
     def __str__(self):
-        return Sumstr
+        return str(Sum.total)
+
 
 class SumThread(threading.Thread):
+    lock = threading.Lock()
 
     def __init__(self, numbers):
+        threading.Thread.__init__(self)
         self.numbers = numbers
-        if not isinstance(numbers, int):
+        if not isinstance(numbers, list):
             raise Exception("numbers is not an array of integers")
 
     def run(self):
-        """compute the sum of all integers inside numbers array"""
+        i = 0
+        for n in self.numbers:
+            i += n
+        SumThread.lock.acquire()
+        Sum.total += i
+        SumThread.lock.release()
