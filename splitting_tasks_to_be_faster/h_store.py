74cb98c5d85c3094f5894ebc0ec1beb2169adc4c
Marine Dejean <marine@Admins-MacBook-Air.local>
2016-07-01 16:26:33 -0700

    added task 2

--- splitting_tasks_to_be_faster/h_store.py
+++ splitting_tasks_to_be_faster/h_store.py
@@ -1,3 +1,24 @@
 import threading
-import time
-import random
+from time import sleep
+from random import randint
+ 
+class Store:
+    
+    def __init__(self, item_number, person_capacity):
+        #number of items available
+        self.item_number = item_number
+        #store capacity
+        self.person_capacity = person_capacity
+        self.semaphore = threading.Semaphore(self.person_capacity)
+
+    def enter(self):
+        self.semaphore.acquire()
+    
+    def buy(self):
+        sleep(randint(5,10))
+        if self.item_number >= 0 :
+            self.item_number -= 1
+        if self.item_number < 0:
+            return False
+        self.semaphore.release()
+        return True
